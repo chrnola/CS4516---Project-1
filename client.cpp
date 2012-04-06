cdc4276cc9d640d9bfd32614d549a4aabcbf3807
Chris Pinola <trogdor3222@gmail.com>
2012-04-06 02:14:08 -0400

    tediously mapping commands to packets

--- client.cpp
+++ client.cpp
@@ -62,7 +62,59 @@ void startPrompt(){
 		} else if(strcmp(argvNew[0], "locations") == 0){
 			Message *m = new Message();
 			m->setCmd("locations");
+			nl -> FromApplicationLayer(m);
+			//TODO: wait for response, display it
 			
+		} else if(strcmp(argvNew[0], "createmissing") == 0){
+			if((argvNew[1] != NULL) && 
+			 (argvNew[2] != NULL) &&
+			 (argvNew[3] != NULL)){
+				Message *m = new Message();
+				int size = strlen(argvNew[0]) + strlen(argvNew[1]) + 
+						strlen(argvNew[2]) + strlen(argvNew[3]);
+				char *cmd = (char *) calloc(size, sizeof(char));
+				strcpy(cmd, argvNew[0]);
+				strcat(cmd, argvNew[1]);
+				strcat(cmd, argvNew[2]);
+				strcat(cmd, argvNew[3]);
+				m->setCmd(cmd);
+				nl -> FromApplicationLayer(m);
+				//TODO: wait for response, display it
+			} else{
+				cout << "Error: createmissing expects firstName lastName lastKnownLocation" << endl;
+			}
+		} else if(strcmp(argvNew[0], "login") == 0){
+			if((argvNew[1] != NULL) && 
+			 (argvNew[2] != NULL)){
+				Message *m = new Message();
+				int size = strlen(argvNew[0]) + strlen(argvNew[1]) + 
+						strlen(argvNew[2]);
+				char *cmd = (char *) calloc(size, sizeof(char));
+				strcpy(cmd, argvNew[0]);
+				strcat(cmd, argvNew[1]);
+				strcat(cmd, argvNew[2]);
+				m->setCmd(cmd);
+				nl -> FromApplicationLayer(m);
+				//TODO: wait for response, display it
+			} else{
+				cout << "Error: login expects username password" << endl;
+			}
+		} else if(strcmp(argvNew[0], "query") == 0){
+			if((argvNew[1] != NULL) && 
+			 (argvNew[2] != NULL)){
+				Message *m = new Message();
+				int size = strlen(argvNew[0]) + strlen(argvNew[1]) + 
+						strlen(argvNew[2];
+				char *cmd = (char *) calloc(size, sizeof(char));
+				strcpy(cmd, argvNew[0]);
+				strcat(cmd, argvNew[1]);
+				strcat(cmd, argvNew[2]);
+				m->setCmd(cmd);
+				nl -> FromApplicationLayer(m);
+				//TODO: wait for response, display it
+			} else{
+				cout << "Error: query expects firstName lastName" << endl;
+			}
 		} else{
 			cout << "Bad command! Try 'help'." << endl;
 		}
